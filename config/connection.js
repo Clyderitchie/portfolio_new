d71fb9428adae18ef76cddce0b51c189395f740d
francis <clyderitchie@yahoo.com>
2023-12-30 21:10:31 -0800

    importing what I need

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,22 @@
-const { connect, connection } = require('mongoose');
+const Sequelize = require('sequelize');
+require('dotenv').config();
 
-const connectionString =
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/portfolioDB';
+let sequelize;
 
-connect(connectionString);
+if (process.env.JAWSDB_URL) {
+    sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+    sequelize = new Sequelize(
+        process.env.DB_NAME,
+        process.env.DB_USER,
+        process.env.DB_PASSWORD,
+        {
+            host: 'localhost',
+            dialect: 'mysql',
+            port: 3306,
+            logging: false
+        }
+    );
+}
 
-module.exports = connection;
+module.exports = sequelize;
